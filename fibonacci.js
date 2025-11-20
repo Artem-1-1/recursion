7424ce337ff3ce769cd817fd5c8ddad2774d23bd
Artem-1-1 <aleksejchukartem@gmail.com>
2025-11-20 10:59:59 +0300

    Added fibonacci recursive with array.

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
 function fibonacci(n) {
   const array = [0, 1];
-  if (n <= 0) return;
+  if (n <= 0) return 'Enter number greater than 0.';
   if (n === 1) return [0];
   if (n === 2) return array;
   for (let i = 2; i < n; i++) {
@@ -9,8 +9,15 @@ function fibonacci(n) {
   return array
 }
 
-console.log(fibonacci(0));
+console.log(fibonacci(5));
 
-function fibonacciRecursive(n) {
-  
-} 
+function fibonacciRecursive(n, sequence = [0, 1]) {
+  if (sequence.length >= n) {
+    return sequence.slice(0 , n)
+  }
+  const newNum = sequence.at(-1) + sequence.at(-2);
+  sequence.push(newNum);
+  return fibonacciRecursive(n, sequence);
+}
+
+console.log(fibonacciRecursive(8));
