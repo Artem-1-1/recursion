beb387c42f26253f0e0f7d7caa0561c93dc18684
Artem-1-1 <aleksejchukartem@gmail.com>
2025-11-20 10:37:06 +0300

    Rename file and rewrite fib fiction with array.

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+function fibonacci(n) {
+  const array = [0, 1];
+  if (n <= 0) return;
+  if (n === 1) return [0];
+  if (n === 2) return array;
+  for (let i = 2; i < n; i++) {
+    array.push(array[array.length-2] + array[array.length-1]);
+  }
+  return array
+}
+
+console.log(fibonacci(0));
+
+function fibonacciRecursive(n) {
+  
+} 
